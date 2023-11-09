ce2d58d4ce02da0936fbe4f1cc52276c0d83814b
Elshan <elshanaliev2009@gmail.com>
2023-11-09 15:58:55 +0500

    style log_reg

--- ShopifyX/Main/urls.py
+++ ShopifyX/Main/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 urlpatterns = [
 
-    path('asd/', home_page, name='home_page'),
+    path('', home_page, name='home_page'),
     path('login/', LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='main/logout.html'), name='logout'),
-    path("", registration, name='register'),
+    path("regiser/", registration, name='register'),
 ]
