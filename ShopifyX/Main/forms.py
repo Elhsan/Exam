b500af5206d2d7be55ba7b287732c54da212a96d
Elshan <elshanaliev2009@gmail.com>
2023-11-10 14:10:01 +0500

    login +

--- ShopifyX/Main/forms.py
+++ ShopifyX/Main/forms.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(required=True,
-                             help_text="We never share your email with anyone else.")
+                             help_text="We never share your email with anyone else.",
+                             widget=forms.EmailInput(attrs={'class': 'email-field'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'username-field'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-field'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-confirm-field'}))
 
     class Meta:
         model = User
-        fields = ('username', 'email',
-                  'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
