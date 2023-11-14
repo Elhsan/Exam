229e61999ffbaa7a8c58c7976b9e5cd4c10c808c
Elshan <elshanaliev2009@gmail.com>
2023-11-14 19:02:05 +0500

    Create Product

--- ShopifyX/Main/forms.py
+++ ShopifyX/Main/forms.py
@@ -14,3 +14,10 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'image', 'description', 'reviews']
--- ShopifyX/Main/urls.py
+++ ShopifyX/Main/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path("regiser/", registration, name='register'),
     path('accounts/', include('allauth.urls')),
     path('accounts/google/login/', include('allauth.urls'), name='google_login'),
-
+    path('product/', product, name='product'),
+    path('product/my_product/', my_products, name='my_product'),
+    path('product/create/', create_product, name='create_product'),
+    path('product/<int:product_id>/', view_product, name='view_product'),
+    path('product/<int:product_id>/edit', edit_product, name='edit_product'),
+    path('product/<int:product_id>/delete/', delete_product, name='delete_product')
 ]
